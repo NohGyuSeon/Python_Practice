aae9e80bac20f4906d9423ba793d7751ff3ccc4c
NGS <kusun1020@gmail.com>
2022-06-11 15:16:41 +0900

    Update Chapter11_05.py

--- Chapter11/Chapter11_05.py
+++ Chapter11/Chapter11_05.py
@@ -1,6 +1,9 @@
 import time
 
-for i in range(10):
-    print("숫자", i, end=", ")
+for i in range(1, 11, 1):
+    if i < 10:
+        print("숫자", i, end=", ")
+    else:
+        print("숫자", i)
     time.sleep(1)
     
